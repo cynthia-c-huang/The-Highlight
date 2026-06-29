1a67c608ebef7983a5a930388357d371ea3e741f
Cynthia Huang <huangcc@usc.edu>
2026-06-29 13:43:28 -0700

    Initial Commit

---
+++ The Highlight/The_HighlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_HighlightApp.swift
+//  The Highlight
+//
+//  Created by Cynthia Huang on 6/29/26.
+//
+
+import SwiftUI
+
+@main
+struct The_HighlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ The HighlightTests/The_HighlightTests.swift
@@ -0,0 +1,18 @@
+//
+//  The_HighlightTests.swift
+//  The HighlightTests
+//
+//  Created by Cynthia Huang on 6/29/26.
+//
+
+import Testing
+
+struct The_HighlightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
